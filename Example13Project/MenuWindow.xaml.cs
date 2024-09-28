249556cdcc433a0a3cabc2f2a8066ab5b880aa5a
2021479 <2021479@TVMLT1596>
2024-09-29 01:00:25 +0530

    Add project files.

---
+++ Example13Project/MenuWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13Project
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        private SumCalcWindow sumCalcWindow = new SumCalcWindow();
+        private SquareOrCubeCalc squareOrCubeCalc = new SquareOrCubeCalc();
+        private MainWindow mainWindow = new MainWindow();
+
+        /*public void ShowChild()
+        {
+            e.Cancel = true;
+            this.Hide();
+        }*/
+        private void btnSquareOrCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            squareOrCubeCalc.Show();
+            squareOrCubeCalc.Activate();
+        }
+
+        private void btnSumCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            sumCalcWindow.Show();
+            sumCalcWindow.Activate();
+        }
+
+       
+
+        private void btnCounter_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Show();
+            mainWindow.Activate();
+        }
+    }
+}
---
+++ Example13Project/SquareOrCubeCalc.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13Project
+{
+    /// <summary>
+    /// Interaction logic for SquareOrCubeCalc.xaml
+    /// </summary>
+    public partial class SquareOrCubeCalc : Window
+    {
+        public SquareOrCubeCalc()
+        {
+            InitializeComponent();
+        }
+        private bool IsNum(string numStr)
+        {
+            try
+            {
+                int.Parse(numStr);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter a number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+
+            int number = int.Parse(txtNumber.Text);
+            int square = number * number;
+            lblResult.Text = "Square";
+            txtResult.Text = square.ToString();
+        }
+
+        private void btnCube_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter a number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+
+            int number = int.Parse(txtNumber.Text);
+            int cube = number * number * number;
+            lblResult.Text = "Cube";
+            txtResult.Text = cube.ToString();
+        }
+    }
+}
