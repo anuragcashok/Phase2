85621d1f67b88b29b04c97613ea251b3744be019
anuragcashok <84575763+anuragcashok@users.noreply.github.com>
2024-10-03 09:19:17 +0530

    Add files via upload

---
+++ CounterApp/CounterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CounterProject
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _counter;
+
+		public int Counter
+		{
+			get { return _counter; }
+			set { _counter = value;OnPropertyChanged("Counter"); }
+		}
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ICommand IncrementCommand { get;}
+        public ICommand DecrementCommand { get;}
+        public CounterViewModel()
+        {
+            IncrementCommand = new RelayCommand(Plus);
+            DecrementCommand = new RelayCommand(Minus);
+        }
+
+        public void Plus()
+        {
+            Counter++;
+        }
+        public void Minus()
+        {
+            Counter--;
+        }
+
+
+    }
+}
